0c5b7979cdf065e2fa35a64f0e972e76be3ec879
teru <85771338+teruteru214@users.noreply.github.com>
2024-10-25 18:13:26 +0900

    Style: Buttonのカスタマイズデザインを追加

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,15 +5,15 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-lg font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0", // rounded-fullに変更
 	{
 		variants: {
 			variant: {
-				default: "bg-primary text-primary-foreground hover:bg-primary/90",
+				default:
+					"border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+				black: "bg-primary text-primary-foreground hover:bg-primary/90",
 				destructive:
 					"bg-destructive text-destructive-foreground hover:bg-destructive/90",
-				outline:
-					"border border-input bg-background hover:bg-accent hover:text-accent-foreground",
 				secondary:
 					"bg-secondary text-secondary-foreground hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -21,9 +21,11 @@ const buttonVariants = cva(
 			},
 			size: {
 				default: "h-10 px-4 py-2",
-				sm: "h-9 rounded-md px-3",
-				lg: "h-11 rounded-md px-8",
-				icon: "h-10 w-10",
+				sm: "h-9 rounded-full px-3",
+				lg: "h-11 rounded-full px-8",
+				xl: "h-12 rounded-full px-10",
+				giant: "h-12 rounded-full px-24",
+				icon: "h-10 w-10 rounded-full",
 			},
 		},
 		defaultVariants: {
