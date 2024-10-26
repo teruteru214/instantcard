f4aae42c3d66156ec024a668b7e383c2fcdf42ab
teru <85771338+teruteru214@users.noreply.github.com>
2024-10-26 16:22:49 +0900

    Style: Footerコンポーネントの変更

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,27 +6,35 @@ const Footer = () => {
 		<footer className="bg-gray-100">
 			<div className="mx-auto max-w-screen-lg px-5 pt-10 pb-28 sm:px-10 sm:flex sm:items-center sm:justify-between">
 				<div className="space-y-5">
-					<span className="cursor-pointer hover:underline">ログアウト</span>
+					<span className="cursor-pointer text-gray-400 hover:text-gray-500 hover:underline">
+						ログアウト
+					</span>
 					<Link href="/" className="flex items-center">
 						<Image src="/logo.webp" alt="title" width={40} height={40} />
 						<h1 className="text-4xl">InstantCard</h1>
 					</Link>
 				</div>
-				<nav className="mt-5 sm:mt-0">
+				<nav className="mt-5 sm:mt-0 text-gray-400">
 					<ul className="space-y-2">
 						<li>
 							<Link href="/#">
-								<span className="hover:underline">使い方</span>
+								<span className="hover:underline hover:text-gray-500">
+									使い方
+								</span>
 							</Link>
 						</li>
 						<li>
 							<Link href="/#">
-								<span className="hover:underline">利用規約</span>
+								<span className="hover:underline hover:text-gray-500">
+									利用規約
+								</span>
 							</Link>
 						</li>
 						<li>
 							<Link href="/#">
-								<span className="hover:underline">プライバシーポリシー</span>
+								<span className="hover:underline hover:text-gray-500">
+									プライバシーポリシー
+								</span>
 							</Link>
 						</li>
 					</ul>
