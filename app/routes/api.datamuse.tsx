bba27be5bae019a0a3355637dc6da83c0d026c23
teru <85771338+teruteru214@users.noreply.github.com>
2024-11-18 12:31:51 +0900

    Refactor: レスポンスの型を作成+リクエストのタイムアウトを作成

--- app/routes/api.datamuse.tsx
+++ app/routes/api.datamuse.tsx
@@ -1,5 +1,10 @@
 import { json } from "@remix-run/node";
 
+interface DatamuseResponse {
+	word: string;
+	score: number;
+}
+
 export const loader = async ({ request }: { request: Request }) => {
 	const url = new URL(request.url);
 	const query = url.searchParams.get("query");
@@ -10,13 +15,16 @@ export const loader = async ({ request }: { request: Request }) => {
 
 	const response = await fetch(
 		`https://api.datamuse.com/words?sp=${encodeURIComponent(query)}*&max=10`,
+		{
+			signal: AbortSignal.timeout(5000),
+		},
 	);
 
 	if (!response.ok) {
 		return json({ suggestions: [] }, { status: response.status });
 	}
 
-	const data = await response.json();
+	const data: DatamuseResponse[] = await response.json();
 
 	return json({ suggestions: data });
 };
