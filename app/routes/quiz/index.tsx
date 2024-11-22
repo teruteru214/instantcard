a6a4c8d9681e6aac5cd3902769cdd5419d342627
teru <85771338+teruteru214@users.noreply.github.com>
2024-11-22 13:03:53 +0900

    Style: クイズデータがないパターンの表示形式を作成

--- app/routes/quiz/index.tsx
+++ app/routes/quiz/index.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigate } from "@remix-run/react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import NoCard from "~/components/global/NoCard";
 import { Button } from "~/components/ui/button";
 import { Form } from "~/components/ui/form";
 import {
@@ -67,48 +68,52 @@ const QuizPage = () => {
 
 	return (
 		<>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-					<QuizHeader
-						isSaveEnabled={form.formState.isDirty && form.formState.isValid}
-						currentAnswerCount={currentAnswerCount}
-						totalQuizCount={enhancedQuizData.length}
-						isFinished={form.formState.isSubmitted}
-					/>
-					{enhancedQuizData.map((quiz, index) => (
-						<QuizCard
-							key={quiz.id}
-							quiz={quiz}
-							options={quiz.options}
-							index={index}
-							scrollToNext={() => scrollToNext(index)}
+			{enhancedQuizData.length === 0 ? (
+				<NoCard type="quiz" />
+			) : (
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+						<QuizHeader
+							isSaveEnabled={form.formState.isDirty && form.formState.isValid}
+							currentAnswerCount={currentAnswerCount}
+							totalQuizCount={enhancedQuizData.length}
+							isFinished={form.formState.isSubmitted}
 						/>
-					))}
-
-					{!form.formState.isSubmitted && (
-						<div className="py-16 space-y-4 flex flex-col items-center justify-center">
-							<img
-								src="/check.webp"
-								alt="confirmation"
-								width={200}
-								height={200}
+						{enhancedQuizData.map((quiz, index) => (
+							<QuizCard
+								key={quiz.id}
+								quiz={quiz}
+								options={quiz.options}
+								index={index}
+								scrollToNext={() => scrollToNext(index)}
 							/>
-							<p>お疲れ様でした！クイズを終了しますか？</p>
-							<Button
-								variant="black"
-								size="giant"
-								disabled={!form.formState.isValid}
-								onClick={() => {
-									scrollToResult();
-								}}
-							>
-								クイズを終了する
-							</Button>
-						</div>
-					)}
-				</form>
-			</Form>
-			{form.formState.isSubmitted && (
+						))}
+
+						{!form.formState.isSubmitted && (
+							<div className="py-16 space-y-4 flex flex-col items-center justify-center">
+								<img
+									src="/check.webp"
+									alt="confirmation"
+									width={200}
+									height={200}
+								/>
+								<p>お疲れ様でした！クイズを終了しますか？</p>
+								<Button
+									variant="black"
+									size="giant"
+									disabled={!form.formState.isValid}
+									onClick={() => {
+										scrollToResult();
+									}}
+								>
+									クイズを終了する
+								</Button>
+							</div>
+						)}
+					</form>
+				</Form>
+			)}
+			{form.formState.isSubmitted && enhancedQuizData.length > 0 && (
 				<Result
 					result={enhancedQuizData.map((quiz, index) => ({
 						word: quiz.word,
