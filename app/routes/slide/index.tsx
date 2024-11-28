81d0060966f3cf87a2188499f70a8cea29f0c4ae
teru <85771338+teruteru214@users.noreply.github.com>
2024-11-28 21:20:51 +0900

    Feat: WordsSlideを作成+スライドメニュー諸々作成

--- app/routes/slide/index.tsx
+++ app/routes/slide/index.tsx
@@ -1,8 +1,12 @@
+import WordsSlide from "./WordsSlide";
+
 const SlidePage = () => {
 	return (
-		<div>
-			<h1>Slide</h1>
-			<input type="text" placeholder="Search..." />
+		<div className="py-4">
+			<WordsSlide />
+			<p className="mt-4 text-center text-gray-400 hover:cursor-pointer hover:underline hover:text-gray-500">
+				スライド機能の使い方→
+			</p>
 		</div>
 	);
 };
