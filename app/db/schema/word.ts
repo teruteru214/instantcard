48590101fb0627dd696ef548bad5636166f80180
teru <85771338+teruteru214@users.noreply.github.com>
2024-12-05 12:48:14 +0900

    Fix: DB構造を変更して、wordを消した際に関連データが消える構造にする

--- app/db/schema/word.ts
+++ app/db/schema/word.ts
@@ -12,7 +12,7 @@ export const words = mysqlTable("words", {
 	id: serial("id").primaryKey(),
 	userId: bigint("user_id", { mode: "number", unsigned: true })
 		.notNull()
-		.references(() => users.id),
+		.references(() => users.id, { onDelete: "cascade" }),
 	word: varchar("word", { length: 50 }).notNull(),
 	position: double("position", { unsigned: true }),
 	translation: varchar("translation", { length: 100 }),
@@ -26,32 +26,18 @@ export const words = mysqlTable("words", {
 
 export const collocations = mysqlTable("collocations", {
 	id: serial("id").primaryKey(),
+	wordId: bigint("word_id", { mode: "number", unsigned: true })
+		.notNull()
+		.references(() => words.id, { onDelete: "cascade" }),
 	text: varchar("text", { length: 100 }).notNull(),
 	translation: varchar("translation", { length: 200 }),
 });
 
-export const wordCollocations = mysqlTable("word_collocations", {
-	id: serial("id").primaryKey(),
-	wordId: bigint("word_id", { mode: "number", unsigned: true })
-		.notNull()
-		.references(() => words.id),
-	collocationId: bigint("collocation_id", { mode: "number", unsigned: true })
-		.notNull()
-		.references(() => collocations.id),
-});
-
 export const examples = mysqlTable("examples", {
 	id: serial("id").primaryKey(),
-	text: varchar("text", { length: 200 }).notNull(),
-	translation: varchar("translation", { length: 300 }),
-});
-
-export const wordExamples = mysqlTable("word_examples", {
-	id: serial("id").primaryKey(),
 	wordId: bigint("word_id", { mode: "number", unsigned: true })
 		.notNull()
-		.references(() => words.id),
-	exampleId: bigint("example_id", { mode: "number", unsigned: true })
-		.notNull()
-		.references(() => examples.id),
+		.references(() => words.id, { onDelete: "cascade" }),
+	text: varchar("text", { length: 200 }).notNull(),
+	translation: varchar("translation", { length: 300 }),
 });
