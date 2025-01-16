15eb3f144af77b96b93e048c618a469cd5075b82
teru <85771338+teruteru214@users.noreply.github.com>
2025-01-16 15:44:03 +0900

    Remove: 音声リクエストをText to Speechに統一するためにコードを破棄

--- app/utils/isWordValid.ts
+++
@@ -1,6 +0,0 @@
-export const isWordValid = (word: string): boolean => {
-	const actualWord = word.trim(); // 本来の文字列を表す変数
-	return (
-		actualWord !== "" && actualWord.length <= 10 && !actualWord.includes(" ")
-	);
-};
